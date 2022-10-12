49eea98e7313e5ca17561e62a5cf437047a335db
Levva01 <vellanifederico13@gmail.com>
2022-10-12 17:03:53 +0200

    Controllers created

---
+++ csharp-boolflix/Controllers/FeaturesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using csharp_boolflix.Data;
+
+namespace csharp_boolflix.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private readonly BoolflixContext _context;
+
+        public FeaturesController(BoolflixContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Features
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Feature.ToListAsync());
+        }
+
+        // GET: Features/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Feature == null)
+            {
+                return NotFound();
+            }
+
+            var feature = await _context.Feature
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feature == null)
+            {
+                return NotFound();
+            }
+
+            return View(feature);
+        }
+
+        // GET: Features/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Features/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Feature feature)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feature);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(feature);
+        }
+
+        // GET: Features/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Feature == null)
+            {
+                return NotFound();
+            }
+
+            var feature = await _context.Feature.FindAsync(id);
+            if (feature == null)
+            {
+                return NotFound();
+            }
+            return View(feature);
+        }
+
+        // POST: Features/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Feature feature)
+        {
+            if (id != feature.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feature);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeatureExists(feature.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(feature);
+        }
+
+        // GET: Features/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Feature == null)
+            {
+                return NotFound();
+            }
+
+            var feature = await _context.Feature
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feature == null)
+            {
+                return NotFound();
+            }
+
+            return View(feature);
+        }
+
+        // POST: Features/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Feature == null)
+            {
+                return Problem("Entity set 'BoolflixContext.Feature'  is null.");
+            }
+            var feature = await _context.Feature.FindAsync(id);
+            if (feature != null)
+            {
+                _context.Feature.Remove(feature);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeatureExists(int id)
+        {
+          return _context.Feature.Any(e => e.Id == id);
+        }
+    }
+}
